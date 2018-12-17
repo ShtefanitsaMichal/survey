b82b53d07bca06f94590f43cec82b37bb4e199fb
ShtefanitsaMichal <mikeshtefanitsa@gmail.com>
2018-12-17 09:12:54 +0100

    Initial commit

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,16 @@
 <?php
-	$servername = "localhost";
-    $username = "root";
-    $password = "root";
+
+    function globals(){
+	 global $username, $servername, $password, $dbname, $conn;	
+    };
+
+    $servername = "localhost";
+    $username = "m.sztefanica";
+    $password = "UphCF4YJj8FAkukh";
     $dbname = "smp_communication_survey";
- 
+
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password );    
+
 ?>
 
 
